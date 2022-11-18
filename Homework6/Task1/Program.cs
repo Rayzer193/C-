e6e3bd28e627273f1adffba9f67a15db4916a80e
Ivan Pavlov <rayzer_s@mail.ru>
2022-11-18 16:35:41 +0300

    homework 6 and 7

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+System.Console.Write("Сколько чисел вы будете вводить?(число от 1 до 100):");
+int dlin = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray(int Length = 100)
+{
+    int[] tempArray = new int[Length];
+    for (int i = 0; i < dlin; i++)
+    {
+        System.Console.Write($"Введите {i+1} число: ");
+        tempArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return tempArray;
+}
+
+void NumbersGreaterZero(int[] tempArray)
+{
+    int count = 0;
+    for(int i = 0; i < dlin; i++)
+    {
+        if(tempArray[i]>0)
+            count = count + 1;
+    }
+    System.Console.WriteLine($"Количество элементов больших нуля: {count}");
+}
+
+NumbersGreaterZero(tempArray: CreateArray());
---
+++ Homework7/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -99,9
+//8 7,8 -7,1 9
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int vys = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int dlin = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Минимальное значение  эелемента массива:");
+int niz = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Максимальное значение элемента массива:");
+int verh = Convert.ToInt32(Console.ReadLine());
+
+double[,] CreatMatrix()
+{ 
+    double[,] matrix = new double[vys,dlin];
+    for (int i = 0; i < vys; i++)
+    { 
+        for (int j = 0; j < dlin; j++)
+        { 
+            matrix[i,j] = new Random().Next(niz, verh) + new Random().NextDouble();
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] printmatrix)
+{ 
+    
+    for (int i = 0; i < vys; i++)
+    { 
+        for (int j = 1; j < dlin; j++)    
+        {
+            Console.Write(String.Format("{0,8}",$"{Math.Round(printmatrix[i, j],1)}|"));
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(printmatrix: CreatMatrix());
