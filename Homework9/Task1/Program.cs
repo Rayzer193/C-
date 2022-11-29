13b8dd14fb488e9ab828e83910737b3e0ff1d1d9
Ivan Pavlov <rayzer_s@mail.ru>
2022-11-29 09:55:09 +0300

    homework9

---
+++ Homework9/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+Console.Clear();
+Console.Write("Задайте первое число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте второе число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintChet (int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    if(m%2==0)
+    {
+        Console.Write(m);
+        if(m<n-1)
+        {
+            Console.Write(',');
+        }
+    }
+    PrintChet(m+1,n);
+}
+Console.Write('"');
+PrintChet(m,n);
+Console.Write('"');
---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+Console.Clear();
+Console.Write("Задайте первое число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте второе число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+void PrintChet (int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    summ += m;
+    PrintChet(m+1,n);
+}
+
+
+PrintChet(m,n);
+Console.WriteLine(summ);
